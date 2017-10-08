7952a192ed6688e1f95930d4eb3c76a5e2cfd31e
odinokun <odinokun@gmail.com>
2017-10-08 15:55:57 +0300

    did anketa all

--- src/js/modules/scroll2ancor.js
+++ src/js/modules/scroll2ancor.js
@@ -6,7 +6,7 @@ module.exports = function() {
        e.preventDefault();
        var anchor = $(this);
        $('html, body').stop().animate({
-        scrollTop: $(anchor.attr('href')).offset().top - 30 + 'px'
+        scrollTop: $(anchor.attr('href')).offset().top - 0 + 'px'
        }, 1000);
     });
     return false;

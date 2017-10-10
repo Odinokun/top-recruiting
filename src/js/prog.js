1b32c78a49e94a09cd513fd8cc3c52cddcbc24c7
odinokun <odinokun@gmail.com>
2017-10-10 10:21:45 +0300

    did popup

--- src/js/prog.js
+++ src/js/prog.js
@@ -59,3 +59,35 @@ $('.header__right-button').on('click', function () {
   $('.header-menu__sublist').slideUp();
 });
 //end open/close header right block
+
+
+// begin popup open
+$('.popup-open').on('click', function() {
+  var popupName = $(this).data('open');
+
+  $('#popup-' + popupName).fadeIn();
+  $('.popup__layer').fadeIn();
+  $('body').addClass('no-scroll');
+
+  return false;
+});
+// end   popup open
+
+// begin popup success
+$('.popup__submit').on('click', function() {
+  $('.popup').fadeOut();
+  $('#popup-success').fadeIn();
+});
+// end   popup success
+
+
+// begin popup close
+$('.popup__layer, .popup-close').on('click', function() {
+  $('.popup, .popup__layer, .popup-success').fadeOut();
+  $('body').removeClass('no-scroll');
+
+  // закрываем попапы в шапке
+  $('.geolocation-region__popup-city, .geolocation-region__popup').fadeOut();
+  return false;
+});
+// end   popup close
